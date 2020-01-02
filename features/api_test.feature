03fa1031a6e70db0322a83587c9c3e629022cb4c
surya teja <surya.teja24@yahoo.com>
2020-01-02 01:33:26 +0000

    initial commit

--- features/api_test.feature
+++ features/api_test.feature
@@ -3,5 +3,9 @@
 Feature: checking the api status
 
     Scenario: Check accepted or rejected status for John smith
-      Given I post the api
-      Then I check for result
+      Given I post the api with John smith json file
+      Then I print the John smith result
+
+    Scenario: Check for Karen Jones
+      Given I post the api with Karen Jones json file
+      Then I print Karen Jones results
--- features/steps/api_test_stepDefs.py
+++ features/steps/api_test_stepDefs.py
@@ -1,23 +1,17 @@
-import http
-from urllib import response
 import json
 import requests
 from behave import given, then
-from nose.tools import assert_true
-from requests.structures import CaseInsensitiveDict
-
-from utils.actions import action_elements
 
 author = 'surya pulakhandam'
 import logging
 
 logging.basicConfig(level=logging.INFO)
 #logging.basicConfig(filename='app.log', filemode='w', level=logging.INFO, format='%(asctime)s - %(message)s')
+api_url = 'http://dummydecisionapi.azurewebsites.net/decision'
 
-@given(u'I post the api')
-def api_post(self):
-    api_url = 'http://dummydecisionapi.azurewebsites.net/decision'
-    with open('application.json') as f:
+@given(u'I post the api with John smith json file')
+def api_john_post(self):
+    with open('john.json') as f:
         payload = json.load(f)
 
     response = requests.post(api_url, json=payload)
@@ -27,13 +21,31 @@ def api_post(self):
     logging.error(response.json())
     logging.critical(response.json())
     api_response = response.json()
-    print(response. headers)
+    print(response.headers)
     return response.headers
 
-@then(u'I check for result')
+@then(u'I print the John smith result')
 def step_impl(context):
-    logging.info(api_post)
-    print(api_post)
+    logging.info(api_john_post)
+    print(api_john_post)
 
 
+@given(u'I post the api with Karen Jones json file')
+def api_karen_post(self):
+    with open('karen.json') as f:
+        payload = json.load(f)
 
+    response_karen = requests.post(api_url, json=payload)
+
+    logging.info(response_karen.json())
+    logging.warning(response_karen.json())
+    logging.error(response_karen.json())
+    logging.critical(response_karen.json())
+    api_response = response_karen.json()
+    print(response_karen.headers)
+    return response_karen.headers
+
+@then(u'I print Karen Jones results')
+def step_impl(context):
+    logging.info(api_karen_post)
+    print(api_karen_post)
